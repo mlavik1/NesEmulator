dc502e50c4540514f775eb1c568815e7f92a364b
Matias Lavik <matias.lavik@hotmail.com>
2017-11-02 22:38:22 +0100

    - APU channels (WIP): Square1, Square2, Triangle

--- src/apu.cpp
+++ src/apu.cpp
@@ -6,31 +6,27 @@
 
 namespace nesemu
 {
-	Sint16 mSampleBuffer[SAMPLE_RATE];
-	size_t mReadPos = 0;
-	size_t mWritePos = SAMPLE_WRITE_BUFFER;
-
-	Sint16 mSampleWriteBuffer[SAMPLE_WRITE_BUFFER];
-	size_t mSampleWriteBufferPos = 0;
-
 	APU::APU()
 	{
-		for (int i = 0; i < SAMPLE_RATE; i++)
-		{
-			//double time = (double)i / (double)SAMPLE_RATE;
-			//double f = sin(2.0f * M_PI * 441.0f * time);
-			mSampleBuffer[i] = 0;
-			//mSampleBuffer[i] = (Sint16)(AMPLITUDE * f);
-
-		}
+		std::fill_n(mSampleBuffer, SAMPLE_RATE, 0);
 	}
 
 	void APU::Tick(int arg_cpucycles)
 	{
+		UpdateBuffer();
+
+		if (!mInitialised)
+		{
+			Initialise();
+		}
+	}
+
+	void APU::UpdateBuffer()
+	{
 		Uint32 currTime = SDL_GetTicks();
 
 		Uint32 timeDiff = currTime - mTimeLastSample;
-		
+
 		if (timeDiff > 0)
 		{
 			const int samplesPerMS = (SAMPLE_RATE / 1000) + 1; // TEMP
@@ -38,21 +34,17 @@ namespace nesemu
 			mTimeLastSample = currTime;
 			for (int i = 0; i < samplesThisFrame; i++)
 			{
-				uint8_t b1 = GMemory->ReadByte(0x4002);
-				uint16_t b2 = GMemory->ReadByte(0x4003);
+				mSampleWriteBuffer[mSampleWriteBufferPos] = 0;
 
-				uint16_t freqVal = b1 | ((b2 << 8) & 0b0000011100000000);
+				const double square1 = GetSquareChannelSampleValue(SquareChannel::One);
+				const double square2 = GetSquareChannelSampleValue(SquareChannel::Two);
+				const double triangle = GetTriangleChannelSampleValue();
+				mSampleWriteBuffer[mSampleWriteBufferPos] += (Sint16)(AMPLITUDE * square1);
+				mSampleWriteBuffer[mSampleWriteBufferPos] += (Sint16)(AMPLITUDE * square2);
+				mSampleWriteBuffer[mSampleWriteBufferPos] += (Sint16)(AMPLITUDE * triangle);
 
-				const double fPulse = (double)freqVal;
-				const double freq = ((1789773.0f) / (16.0f * (fPulse + 1.0f))) - 1.0f;
-				//const double freq = (441.0f + (mTime*4.0f));
+				mSampleWriteBufferPos++;
 
-				const double f = sin(2.0f * M_PI * (freq) * mTime);
-				const double fRel = (f + 1.0f) / 2.0f;
-				const int iSample = (fRel * 8);
-				
-				mSampleWriteBuffer[mSampleWriteBufferPos++] = (Sint16)(AMPLITUDE * (iSample >= 6 ? 1.0f : -1.0f));
-				
 				// Copy to sample buffer
 				if (mSampleWriteBufferPos >= SAMPLE_WRITE_BUFFER)
 				{
@@ -78,51 +70,99 @@ namespace nesemu
 				mTime = (double)mSampleCounter / (double)SAMPLE_RATE;
 			}
 		}
+	}
 
-		if (!mInitialised)
+	void APU::Initialise()
+	{
+		mInitialised = true;
+
+		mTimeLastSample = SDL_GetTicks();
+
+		int sample_nr = 0;
+
+		SDL_AudioSpec audioSpec;
+		audioSpec.freq = SAMPLE_RATE; // samples per second
+		audioSpec.format = AUDIO_S16SYS; // signed short (16 bit)
+		audioSpec.channels = 1;
+		audioSpec.samples = 512; // buffer-size
+		audioSpec.callback = audio_callback;
+		audioSpec.userdata = this;
+
+		SDL_AudioSpec desiredSpec;
+		if (SDL_OpenAudio(&audioSpec, &desiredSpec) != 0)
+			SDL_LogError(SDL_LOG_CATEGORY_AUDIO, "Failed to open audio: %s", SDL_GetError());
+		if (audioSpec.format != desiredSpec.format)
+			SDL_LogError(SDL_LOG_CATEGORY_AUDIO, "Failed to get the desired AudioSpec");
+
+		SDL_PauseAudio(0);
+	}
+
+	double APU::GetSquareChannelSampleValue(const SquareChannel arg_channel)
+	{
+		// read frequency from memory
+		const uint8_t b1 = GMemory->ReadByte(arg_channel == SquareChannel::One ? 0x4000 : 0x4004);
+		const uint8_t b2 = GMemory->ReadByte(arg_channel == SquareChannel::One ? 0x4001 : 0x4005);
+		const uint8_t b3 = GMemory->ReadByte(arg_channel == SquareChannel::One ? 0x4002 : 0x4006);
+		const uint8_t b4 = GMemory->ReadByte(arg_channel == SquareChannel::One ? 0x4003 : 0x4007);
+
+		uint16_t freqVal = b3 | (((uint16_t)b4 << 8) & 0b0000011100000000);
+
+		if (freqVal > 0)
 		{
-			mInitialised = true;
+			const uint8_t duty = b1 >> 6; // first two bits of $4000 / $4004
+			const double fPulse = (double)freqVal;
+			const double freq = ((1789773.0f) / (16.0f * (fPulse + 1.0f))) - 1.0f;
 
-			mTimeLastSample = SDL_GetTicks();
+			const double f = sin(2.0f * M_PI * (freq)* mTime);
+			const double fRel = (f + 1.0f) / 2.0f;
+			const int iSample = (fRel * 8);
 
-			int sample_nr = 0;
+			return SquareDutyChannel[duty][iSample];
+		}
+		return 0.0f;
+	}
 
-			SDL_AudioSpec audioSpec;
-			audioSpec.freq = SAMPLE_RATE; // samples per second
-			audioSpec.format = AUDIO_S16SYS; // signed short (16 bit)
-			audioSpec.channels = 1;
-			audioSpec.samples = 512; // buffer-size
-			audioSpec.callback = audio_callback;
-			audioSpec.userdata = &sample_nr;
+	double APU::GetTriangleChannelSampleValue()
+	{
+		// read sound data from memory
+		const uint8_t b1 = GMemory->ReadByte(0x4008);
+		const uint8_t b2 = GMemory->ReadByte(0x400A);
+		const uint8_t b3 = GMemory->ReadByte(0x400B);
 
-			SDL_AudioSpec desiredSpec;
-			if (SDL_OpenAudio(&audioSpec, &desiredSpec) != 0)
-				SDL_LogError(SDL_LOG_CATEGORY_AUDIO, "Failed to open audio: %s", SDL_GetError());
-			if (audioSpec.format != desiredSpec.format)
-				SDL_LogError(SDL_LOG_CATEGORY_AUDIO, "Failed to get the desired AudioSpec");
+		uint16_t freqVal = b2 | (((uint16_t)b3 << 8) & 0b0000011100000000);
 
-			SDL_PauseAudio(0);
+		if (freqVal > 0)
+		{
+			const double fPulse = (double)freqVal;
+			const double freq = ((1789773.0f) / (16.0f * (fPulse + 1.0f))) - 1.0f;
+
+			const double f = sin(2.0f * M_PI * (freq)* mTime);
+			const double fRel = (f + 1.0f) / 2.0f;
+			const int iSample = (fRel * 32);
+
+			return TriangleDutyCycleSequence[iSample];
 		}
 	}
 
 	void APU::audio_callback(void *user_data, Uint8 *raw_buffer, int bytes)
 	{
+		APU* apu = (APU*)user_data;
 		Sint16 *buffer = (Sint16*)raw_buffer;
 		int length = bytes / 2; // 2 bytes per sample for AUDIO_S16SYS
 		
 		const size_t totalLen = bytes / 2;
-		const size_t lenToEnd = SAMPLE_RATE - mReadPos;
+		const size_t lenToEnd = SAMPLE_RATE - apu->mReadPos;
 		const size_t len = lenToEnd > totalLen ? totalLen : lenToEnd;
 		const size_t lenBytes = len * 2;
 
-		memcpy(buffer, mSampleBuffer + mReadPos,  lenBytes);
-		mReadPos += totalLen;
-		if (mReadPos >= SAMPLE_RATE)
+		memcpy(buffer, apu->mSampleBuffer + apu->mReadPos,  lenBytes);
+		apu->mReadPos += totalLen;
+		if (apu->mReadPos >= SAMPLE_RATE)
 		{
-			mReadPos -= SAMPLE_RATE;
-			if (mReadPos > 0)
+			apu->mReadPos -= SAMPLE_RATE;
+			if (apu->mReadPos > 0)
 			{
-				memcpy(buffer + len, mSampleBuffer, mReadPos * 2);
+				memcpy(buffer + len, apu->mSampleBuffer, apu->mReadPos * 2);
 			}
 		}
 	}
--- src/apu.h
+++ src/apu.h
@@ -11,6 +11,12 @@ const int SAMPLE_WRITE_BUFFER = 768; // 512 + 256
 
 namespace nesemu
 {
+	enum SquareChannel
+	{
+		One,
+		Two
+	};
+
 	class APU
 	{
 	private:
@@ -20,9 +26,33 @@ namespace nesemu
 		uint64_t mSampleCounter = 0;
 		double mTime = 0.0f;
 
+		Sint16 mSampleBuffer[SAMPLE_RATE];
+		size_t mReadPos = 0;
+		size_t mWritePos = SAMPLE_WRITE_BUFFER;
+
+		Sint16 mSampleWriteBuffer[SAMPLE_WRITE_BUFFER];
+		size_t mSampleWriteBufferPos = 0;
+
+		int TriangleDutyCycleSequence[32] =
+		{ 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7
+		 -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+
+		int SquareDutyChannel[4][8] = 
+		{
+			{ -4, -4, -4, -4, -4, -4, -4,  4 },
+			{ -4, -4, -4, -4, -4, -4,  4,  4 },
+			{ -4, -4, -4, -4,  4,  4,  4,  4 },
+			{  4,  4,  4,  4,  4,  4, -4, -4 }
+		};
+
 	public:
 		APU();
 		void Tick(int arg_cpucycles);
+		void Initialise();
+		void UpdateBuffer();
+
+		double GetSquareChannelSampleValue(const SquareChannel arg_channel);
+		double GetTriangleChannelSampleValue();
 
 		static void audio_callback(void *user_data, Uint8 *raw_buffer, int bytes);
 	};
